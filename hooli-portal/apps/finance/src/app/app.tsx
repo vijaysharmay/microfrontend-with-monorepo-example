b0941b858c3b85783109a9cb088b570c01200a60
Vijay Y <vijaysharma.yellepeddi@gmail.com>
2025-04-02 08:47:30 +0530

    Adding vercel config files

--- hooli-portal/apps/finance/src/app/app.tsx
+++ hooli-portal/apps/finance/src/app/app.tsx
@@ -7,8 +7,15 @@ import {
   PersonMoney20Filled,
 } from '@fluentui/react-icons';
 import { SideNav, SideNavItem } from '@hooli-portal/uicomponents';
-import { Switch } from 'wouter';
-import Router from './router';
+import { Route, Switch } from 'wouter';
+import NewVendor from './pages/accounting/new-vendor';
+import ReceivablesInvoices from './pages/accounting/receivables-invoices';
+import ReceivablesKPIs from './pages/accounting/receivables-kpis';
+import ReviewPurchaseOrders from './pages/accounting/review-purchase-orders';
+import Home from './pages/home';
+import InventoryManagement from './pages/inventory-management';
+import NotificationsCenter from './pages/notifications-center';
+import Reports from './pages/reports';
 
 export function App() {
   const navItems: SideNavItem[] = [
@@ -45,7 +52,14 @@ export function App() {
   return (
     <SideNav heading="Hooli Finance" navItems={navItems}>
       <Switch>
-        <Router attachPrefixFlag={false} />
+        <Route path={'/'} component={Home} />
+        <Route path={`/reports`} component={Reports} />
+        <Route path={`/notifications`} component={NotificationsCenter} />
+        <Route path={`/receivables/kpis`} component={ReceivablesKPIs} />
+        <Route path={`/receivables/invoices`} component={ReceivablesInvoices} />
+        <Route path={`/payables/review`} component={ReviewPurchaseOrders} />
+        <Route path={`/payables/vendor-new`} component={NewVendor} />
+        <Route path={`/inventory`} component={InventoryManagement} />
       </Switch>
     </SideNav>
   );
--- hooli-portal/apps/finance/src/app/router.tsx
+++ hooli-portal/apps/finance/src/app/router.tsx
@@ -9,27 +9,16 @@ import NotificationsCenter from './pages/notifications-center';
 import Reports from './pages/reports';
 
 export default function Router({ attachPrefixFlag = true }) {
-  const prefix = attachPrefixFlag ? 'finance/' : '';
-  console.log('finance', attachPrefixFlag);
   return (
     <>
-      <Route
-        path={attachPrefixFlag ? `/${prefix}home` : '/'}
-        component={Home}
-      />
-      <Route path={`/${prefix}reports`} component={Reports} />
-      <Route path={`/${prefix}notifications`} component={NotificationsCenter} />
-      <Route path={`/${prefix}receivables/kpis`} component={ReceivablesKPIs} />
-      <Route
-        path={`/${prefix}receivables/invoices`}
-        component={ReceivablesInvoices}
-      />
-      <Route
-        path={`/${prefix}payables/review`}
-        component={ReviewPurchaseOrders}
-      />
-      <Route path={`/${prefix}payables/vendor-new`} component={NewVendor} />
-      <Route path={`/${prefix}inventory`} component={InventoryManagement} />
+      <Route path={'/'} component={Home} />
+      <Route path={`/reports`} component={Reports} />
+      <Route path={`/notifications`} component={NotificationsCenter} />
+      <Route path={`/receivables/kpis`} component={ReceivablesKPIs} />
+      <Route path={`/receivables/invoices`} component={ReceivablesInvoices} />
+      <Route path={`/payables/review`} component={ReviewPurchaseOrders} />
+      <Route path={`/payables/vendor-new`} component={NewVendor} />
+      <Route path={`/inventory`} component={InventoryManagement} />
     </>
   );
 }
